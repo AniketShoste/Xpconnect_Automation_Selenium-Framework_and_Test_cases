873850d6597a2c0a3228d5b9530295f95f889f3a
saurabhranadive <sranadive@xpediantsolutions.com>
2023-06-09 17:38:38 +0530

    refactor

--- pages/aem_appsetting.py
+++ pages/aem_appsetting.py
@@ -16,9 +16,9 @@ class AEM_Appsetting(BaseSetup):
     '''
           Locators and methods of the page written on same page - This is part of page object model
     '''
-    xpconnect_content = (By.CSS_SELECTOR,"[data-foundation-collection-item-id='\/content\/XpConnect']")
-    xp_appsetting = (By.CSS_SELECTOR,"[data-foundation-collection-item-id='\/content\/XpConnect\/application-settings'] coral-columnview-item-thumbnail")
-    xp_edit_button = (By.CSS_SELECTOR, "coral-actionbar-item:nth-of-type(2)  coral-button-label")
+    xpconnect_content = (By.XPATH,"//div[@id='granite-shell-content']//coral-columnview[@role='tree']/coral-columnview-column[@role='presentation']/coral-columnview-column-content[@role='presentation']/coral-columnview-item[15]")
+    xp_appsetting = (By.XPATH,"//div[@id='granite-shell-content']//coral-columnview[@role='tree']/coral-columnview-column[@role='group']/coral-columnview-column-content[@role='presentation']/coral-columnview-item[1]/coral-columnview-item-thumbnail")
+    xp_edit_button = (By.XPATH, "//coral-actionbar[@role='region']/coral-actionbar-primary[@role='toolbar']/coral-actionbar-item[2]/button[@type='button']/coral-button-label")
     frame_demo = (By.CSS_SELECTOR, "iframe#ContentFrame")
     demo_dashboard = (By.CSS_SELECTOR,".navbar-nav.mr-auto > .nav-item:nth-child(3) .nav-link")
     veeva_credentials = (By.CSS_SELECTOR,".container > .title")
@@ -28,45 +28,50 @@ class AEM_Appsetting(BaseSetup):
 
 
     def click_xpconnect_content(self):
+        time.sleep(2)
         self.seleniumutil.wait_for_element_visible(self.xpconnect_content)
         self.seleniumutil.click(*self.xpconnect_content)
         print("Click on Xpconnect from AEM")
 
     def click_app_setting(self):
+        time.sleep(2)
         self.seleniumutil.wait_for_element_visible(self.xp_appsetting)
         self.seleniumutil.click(*self.xp_appsetting)
         print("Click on Xpconnect application setting")
 
     def click_xp_edit_button(self):
-        time.sleep(3)
+        time.sleep(2)
         self.seleniumutil.wait_for_element_clickable(self.xp_edit_button)
         self.seleniumutil.click(*self.xp_edit_button)
         print("Click on Xpconnect application setting")
 
     def switch_to_appsetting_frame(self):
-        time.sleep(3)
+        time.sleep(2)
         self.seleniumutil.wait_for_element_visible(self.frame_demo)
         self.seleniumutil.switch_to_frame(*self.frame_demo)
         print("Switched frame")
 
     def click_demo_dashboard(self):
-        time.sleep(3)
+        time.sleep(2)
         self.seleniumutil.wait_for_element_visible(self.demo_dashboard)
         self.seleniumutil.click_using_js(*self.demo_dashboard)
         print("Clicked on demo dashboard")
     def click_veeva_credentials(self):
+        time.sleep(2)
         self.seleniumutil.wait_for_element_visible(self.veeva_credentials)
         print("Waited for element")
         self.seleniumutil.click_using_js(*self.veeva_credentials)
         print("clicked on veeva credentails")
 
     def verify_veeva_url(self):
+        time.sleep(2)
         self.seleniumutil.wait_for_element_visible(self.fetch_veeva_url)
         veevaurl = self.seleniumutil.fetch_using_js(*self.fetch_veeva_url)
         self.propertyutil.set_property("veeva_url_validate", veevaurl)
         print("Captured veeva url from application setting", veevaurl)
 
     def verify_veeva_username(self):
+        time.sleep(2)
         self.seleniumutil.wait_for_element_visible(self.fetch_veeva_username)
         veevausername = self.seleniumutil.fetch_using_js(*self.fetch_veeva_username)
         self.propertyutil.set_property("veeva_username_validate", veevausername)
